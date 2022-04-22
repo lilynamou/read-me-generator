b283066d80bfed8084d8d083d7a7d3803ce62225
lilynamou <lilyananamou@gmail.com>
2022-04-21 22:06:18 -0700

    updating git ignore and index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require('fs');
+const util = require("util");
 
 // TODO: import the generateMarkdown function from utils/generateMarkdown.js
 
 // TODO: Write code to get user input, generate markdown, and save it to a file.
+//QUESTON ARRAY
+
+const questions = [{
+    type: "input",
+    message: ""
+    name:
+}]
